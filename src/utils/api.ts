3eabe62d408d9da7689c6a6be4f81b3c703e0a1e
sain419 <lildestro7@gmail.com>
2026-06-13 02:45:45 +0100

    fix: add vercel routing config

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001/api',
+  baseURL: 'https://payflex-api.onrender.com/api',
 })
 
 // Automatically attach token to every request
